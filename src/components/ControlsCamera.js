0940888d92404eaf2f2ee81f9a2b2ad4ab1a4b51
acamiloforero <161332422+acamiloforero@users.noreply.github.com>
2025-03-14 17:46:19 -0500

    disble zoom

--- src/components/ControlsCamera.js
+++ src/components/ControlsCamera.js
@@ -3,6 +3,7 @@ import { OrbitControls } from "@react-three/drei";
 const ControlsCamera = () => {
   return (
     <OrbitControls
+      enableZoom={false} // Deshabilita el zoom
       minPolarAngle={0} // Permite ver desde arriba completamente
       maxPolarAngle={Math.PI / 2.5} // Restringe para que no baje por debajo del auto
     />
